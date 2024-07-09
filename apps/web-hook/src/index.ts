3c8c6949462f387ce9a9944745e2513b9803ac65
Vijay Ravichander <vijaypreetham1@gmail.com>
2024-07-08 17:55:59 -0700

    .

---
+++ apps/web-hook/src/index.ts
@@ -0,0 +1,68 @@
+import express, { Request, Response } from 'express';
+import { db } from './db';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    return res.status(200).json({ message: 'Healthy' });
+});
+
+app.put('/judge0', async (req: Request, res: Response) => {
+    try {
+        const body = req.body;
+
+        const submissionTokens:{ token: string, status: { id: number } }[] = [body]
+
+        const firstToken = submissionTokens[0].token;
+
+        console.log(firstToken)
+
+        const submission = await db.submissions.findFirst({
+            where: {
+                //@ts-ignore
+                judge0Token: firstToken
+            }
+        });
+
+        console.log(submission)
+
+        for (const sub of submissionTokens) {
+            if (sub.status.id !== 3) {
+                await db.submissions.update({
+                    where: {
+                        //@ts-ignore
+                        id: submission.id
+                    },
+                    data: {
+                        status: 'REJECTED'
+                    },
+                });
+
+                return res.status(200).json({});
+            }
+        }
+
+        await db.submissions.update({
+            where: {
+                //@ts-ignore
+                id: submission.id
+            },
+            //@ts-ignore
+            data: {
+                status: 'ACCEPTED'
+            }
+        });
+
+        return res.status(200).json({});
+
+
+    } catch (error) {
+        console.error('Error updating submission status:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.listen(3005, () => {
+    console.log('App started on port: 3005');
+});
